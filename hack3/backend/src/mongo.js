59ca15a6d989492f11b204e005d0830d67c41daf
matdexir <matthieudesir21@gmail.com>
2021-12-28 09:06:48 +0800

    Quick save

---
+++ hack3/backend/src/mongo.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { dataInit } from "./upload.js";
+
+import "dotenv-defaults/config.js";
+
+async function connect() {
+  // TODO 1.1 Connect your MongoDB
+}
+
+export default { connect };
--- hack3/frontend/src/index.js
+++ hack3/frontend/src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  split,
+  HttpLink,
+} from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
+
+// TODO 1.2 Connect your GraphQL Server
+
+const client = new ApolloClient({
+  // link: splitLink, // TODO 1.2 Uncomment this while you finish the splitLink
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
