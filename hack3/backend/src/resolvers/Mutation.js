e7114da5e38baae56a07d7079fcead606b3e6378
matdexir <matthieudesir21@gmail.com>
2021-12-28 10:25:22 +0800

    Task 4 done

--- hack3/backend/src/resolvers/Mutation.js
+++ hack3/backend/src/resolvers/Mutation.js
@@ -33,6 +33,13 @@ const Mutation = {
    * Delete a task by id
    */
   // TODO 5.2 Add a deleteTask function to resolve deleteTask
+  deleteTask: async (parent, { id }, { taskModel, pubSub }) => {
+    await taskModel.findOneAndDelete({ id });
+    // pubSub.publish("TASK_DELETED", {
+    // taskDeleted: task,
+    // });
+    return id;
+  },
   // TODO 6.3 Add Subscription
 };
 
